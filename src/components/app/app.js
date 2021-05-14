d62ebd77454a9b88f439b39cb656025260d14636
Marian Lyzhychka <mr.lezhichka@gmail.com>
2021-05-14 17:27:54 +0300

    first commit

---
+++ src/components/app/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import style from '../../assets/style/scss/app/app.module.css';
+
+const App = () => {
+    return (
+        <div className={style.main}>
+
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/header/header-navbar/header-navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeaderNavbar = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default HeaderNavbar;
