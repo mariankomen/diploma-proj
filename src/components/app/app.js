afb561a6cb563a752677a9ba30a4e919f340e46f
Marian Lyzhychka <mr.lezhichka@gmail.com>
2021-05-19 11:41:12 +0300

    rele and modems completed

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,6 +14,10 @@ import Condensators from "../condensators/condensators";
 import CondensatorsInfoPage from "../condensators/condensators-info-page/condensators-info-page";
 import Transistors from "../transistors/transistors";
 import TransistorsInfoPage from "../transistors/transistors-info-page/transistors-info-page";
+import Rele from "../rele/rele";
+import ReleInfoPage from "../rele/rele-info-page/rele-info-page";
+import Modems from "../modems/modems";
+import ModemInfoPage from "../modems/modem-info-page/modem-info-page";
 
 const App = () => {
 
@@ -22,6 +26,8 @@ const App = () => {
     const data_res = require('../../data/resistors.json')
     const data_cond = require('../../data/condensators.json')
     const data_tran = require('../../data/transistors.json')
+    const data_rele = require('../../data/rele.json')
+    const data_modem = require('../../data/modems.json')
     const [arr,setArr] = useState([])
 
 
@@ -50,6 +56,18 @@ const App = () => {
                 <Route path='/catalog/transistors/:id' render={() => <TransistorsInfoPage  data={data_tran[resID]}
                                                                                             arr={arr}
                                                                                             setArr={setArr}
+                />}  />
+
+                <Route path='/catalog/rele'  exact render={() => <Rele setResID={setResID}/>}/>
+                <Route path='/catalog/rele/:id' render={() => <ReleInfoPage  data={data_rele[resID]}
+                                                                             arr={arr}
+                                                                             setArr={setArr}
+                />}  />
+
+                <Route path='/catalog/modems'  exact render={() => <Modems setResID={setResID}/>}/>
+                <Route path='/catalog/modems/:id' render={() => <ModemInfoPage  data={data_modem[resID]}
+                                                                                arr={arr}
+                                                                                setArr={setArr}
                 />}  />
 
             </BrowserRouter>
